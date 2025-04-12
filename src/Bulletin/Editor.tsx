4f099c18b614ad9c16a29344a08f0d89a7939e63
Hieu Nguyen <hieun201104@gmail.com>
2025-04-12 11:51:55 -0700

    Moved data and form to admin folder

--- src/Bulletin/Editor.tsx
+++ src/Bulletin/Editor.tsx
@@ -1,6 +1,6 @@
 import "./editor-styles.css";
 import { EditorProvider } from "@tiptap/react";
-import { extensions } from "../User/Form/EditorExtensions";
+import { extensions } from "../Admin/Form/EditorExtensions";
 export default function Editor({ content }: { content: string }) {
   return (
     <EditorProvider
@@ -10,8 +10,8 @@ export default function Editor({ content }: { content: string }) {
       editorProps={{
         attributes: {
           class:
-            "border-black border rounded-standard p-5  focus:outline-none min-h-[70vh] overflow-y-scroll"
-        }
+            "border-black border rounded-standard p-5  focus:outline-none min-h-[70vh] overflow-y-scroll",
+        },
       }}
     ></EditorProvider>
   );
