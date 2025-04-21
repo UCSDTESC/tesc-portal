1217396e4697607fa0ec36c2a9688a966acea443
Hieu <hieun201104@gmail.com>
2025-04-20 19:16:04 -0700

    Datatable: refactor EditModal and DataPair into individual components

--- src/Admin/Data/DataTable.tsx
+++ src/Admin/Data/DataTable.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { ReactNode, useContext, useEffect, useState } from "react";
 import UserContext from "../../UserContext";
 import { EditorProvider } from "@tiptap/react";
 import { extensions } from "../Form/EditorExtensions";
@@ -16,20 +16,22 @@ export default function DataTable() {
   const [currEdit, setCurrEdit] = useState<formdata>(eventFormDataDefault);
 
   // fetch events posted by user
-  useEffect(() => {
+  const fetchData = async () => {
     if (!User) {
       return;
     }
-    const fetchData = async () => {
-      const { data, error } = await fetchEventByOrg(User.id);
-      if (data) {
-        setData(data);
-      } else if (error) {
-        console.log(error);
-      }
-    };
+    const { data, error } = await fetchEventByOrg(User.id);
+    if (data) {
+      setData(data);
+    } else if (error) {
+      console.log(error);
+    }
+  };
+
+  // fetch events posted by user on component render
+  useEffect(() => {
     fetchData();
-  }, [User, showEditModal]);
+  }, [User]);
 
   // delete event
   const handleDelete = async (id: number) => {
@@ -42,7 +44,7 @@ export default function DataTable() {
   };
 
   // Open the Edit Modal with the corresponding data inserted in
-  const setEdit = (daton: Event) => {
+  const openEditModal = (daton: Event) => {
     setShowEditModal(true);
     setCurrID(daton.id);
     setCurrEdit({
@@ -56,95 +58,105 @@ export default function DataTable() {
       tags: daton.tags
     });
   };
+  if (data)
+    return (
+      <main className="grid w-1/2 gap-2">
+        {data.map((daton) => {
+          return (
+            <span className="border-black border rounded-lg w-full p-5  relative" key={daton.id}>
+              <button
+                className="absolute right-[-15px] top-[-15px] rounded-lg p-2 text-white bg-red-600 w-fit hover:bg-red-800 cursor-pointer"
+                onClick={() => {
+                  handleDelete(daton.id);
+                }}>
+                Delete
+              </button>
+              <button
+                className="absolute right-[50px] top-[-15px] rounded-lg p-2 text-black bg-gray-300 w-fit hover:bg-gray-400 cursor-pointer"
+                onClick={() => {
+                  openEditModal(daton);
+                }}>
+                edit
+              </button>
+              <div className="w-full grid grid-cols-[auto_1fr] gap-x-2 ">
+                <DataPair data={daton.title}>Title</DataPair>
+                <DataPair data={daton.password}>Password</DataPair>
+                <DataPair data={DateParser(daton.created_at)}>Time Stamp</DataPair>
+                <DataPair data={DateParser(daton.start_date)}>Start Date</DataPair>
 
+                <DataPair data={DateParser(daton.end_date)}>End Date</DataPair>
+                <DataPair data={daton.location_str}>Location</DataPair>
+                <DataPair data={daton.rsvp}>rsvp:</DataPair>
+                <DataPair data={daton.tags ? daton.tags.join(", ") : ""}>tags:</DataPair>
+              </div>
+              <EditorProvider
+                extensions={extensions}
+                content={daton.content}
+                editable={false}
+                editorProps={{
+                  attributes: {
+                    class:
+                      "border-black border rounded-lg p-3 w-full col-span-2 focus:outline-none max-h-[40vh]  overflow-y-auto"
+                  }
+                }}></EditorProvider>
+            </span>
+          );
+        })}
+        {showEditModal &&
+          createPortal(
+            <EditModal {...{ setShowEditModal, fetchData, currEdit, curID }} />,
+            document.body
+          )}
+      </main>
+    );
+}
+
+// Edit modal that is shown when User clicks on edit for a particular data object
+function EditModal({
+  setShowEditModal,
+  fetchData,
+  currEdit,
+  curID
+}: {
+  setShowEditModal: (ShowEditModal: boolean) => void;
+  currEdit: formdata;
+  fetchData: () => void;
+  curID: number;
+}) {
+  return (
+    <div className="w-screen h-screen fixed top-0 flex justify-center items-center z-100 overflow-scroll">
+      <div
+        className="fixed top-0 w-full h-full bg-black opacity-35 cursor-pointer"
+        onClick={() => {
+          setShowEditModal(false);
+        }}
+      />
+      <Form
+        formdata={currEdit}
+        id={curID}
+        onSuccess={() => {
+          setShowEditModal(false);
+          fetchData();
+        }}
+      />
+    </div>
+  );
+}
+
+// Label-Data pair displayed on
+function DataPair({
+  children,
+  data,
+  className
+}: {
+  children: ReactNode;
+  data: string | number;
+  className?: string;
+}) {
   return (
     <>
-      {data && (
-        <div className="grid w-1/2 gap-2">
-          {data.map((daton) => {
-            console.log(daton.content);
-            return (
-              <span
-                className="border-black border rounded-lg w-full p-5  relative"
-                key={daton.id}
-              >
-                <button
-                  className="absolute right-[-15px] top-[-15px] rounded-lg p-2 text-white bg-red-600 w-fit hover:bg-red-800 cursor-pointer"
-                  onClick={() => {
-                    handleDelete(daton.id);
-                  }}
-                >
-                  Delete
-                </button>
-                <button
-                  className="absolute right-[50px] top-[-15px] rounded-lg p-2 text-black bg-gray-300 w-fit hover:bg-gray-400 cursor-pointer"
-                  onClick={() => {
-                    setEdit(daton);
-                  }}
-                >
-                  edit
-                </button>
-                <div className="w-full grid grid-cols-[auto_1fr] gap-x-2 ">
-                  <label className="">Title</label>
-                  <div className=""> {daton.title}</div>
-                  <label className="">Password</label>
-                  <div className=""> {daton.password}</div>
-                  <label className="">Time Stamp</label>
-                  <div className=""> {DateParser(daton.created_at)}</div>
-                  <label htmlFor="">Start Date</label>
-                  <div className=" whitespace-pre-line">
-                    {DateParser(daton.start_date)}
-                  </div>
-                  <label htmlFor="">End Date</label>
-                  <div className=" whitespace-pre-line">
-                    {DateParser(daton.end_date)}
-                  </div>
-                  <label htmlFor="">Location</label>
-                  <div className=" whitespace-pre-line">
-                    {daton.location_str}
-                  </div>
-                  <label htmlFor="">rsvp: </label>
-                  <div className=" whitespace-pre-line">{daton.rsvp}</div>
-                  <label htmlFor="">tags: </label>
-                  <div className=" whitespace-pre-line">
-                    {daton.tags ? daton.tags.join(", ") : ""}
-                  </div>
-                </div>
-                <EditorProvider
-                  extensions={extensions}
-                  content={daton.content}
-                  editable={false}
-                  editorProps={{
-                    attributes: {
-                      class:
-                        "border-black border rounded-lg p-3 w-full col-span-2 focus:outline-none max-h-[40vh]  overflow-y-auto"
-                    }
-                  }}
-                ></EditorProvider>
-              </span>
-            );
-          })}
-        </div>
-      )}
-      {showEditModal &&
-        createPortal(
-          <div className="w-screen h-screen fixed top-0 flex justify-center items-center z-100 overflow-scroll">
-            <div
-              className="fixed top-0 w-full h-full bg-black opacity-35 cursor-pointer"
-              onClick={() => {
-                setShowEditModal(false);
-              }}
-            ></div>
-            <Form
-              formdata={currEdit}
-              id={curID}
-              onSuccess={() => {
-                window.location.href = "";
-              }}
-            ></Form>
-          </div>,
-          document.body
-        )}
+      <label className="">{children}</label>
+      <div className={className}> {data}</div>
     </>
   );
 }
