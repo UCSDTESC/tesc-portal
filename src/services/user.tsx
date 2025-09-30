7f66039267e818f71c7434f06c3db912689c133f
Hieu <vin028@ucsd.edu>
2025-09-30 12:51:20 -0700

    Fix: nvm actually fixed now

--- src/services/user.tsx
+++ src/services/user.tsx
@@ -3,21 +3,21 @@ import supabase from "@server/supabase";
 export const signIn = async (email: string, password: string) => {
   const { data, error } = await supabase.auth.signInWithPassword({
     email: email,
-    password: password
+    password: password,
   });
   if (error) return { user: null, error };
   const { data: role } = await supabase.from("Users").select("role").eq("email", data.user.email);
   const user = {
     id: data.user.id,
     email: data.user.email,
-    role: role ? role[0].role : "unknown"
+    role: role ? role[0].role : "unknown",
   };
   return { user, error };
 };
 
 export const fetchUser = async () => {
   const {
-    data: { user }
+    data: { user },
   } = await supabase.auth.getUser();
   const { data: role } = await supabase.from("Users").select("role").eq("email", user?.email);
   if (user && role) return { id: user.id, email: user.email, role: role[0].role };
@@ -32,7 +32,7 @@ export const signUp = async (email: string, password: string) => {
   // add user to auth table
   const { data, error } = await supabase.auth.signUp({
     email: email,
-    password: password
+    password: password,
   });
   if (error) return { user: null, error };
 
@@ -59,7 +59,6 @@ export const verifyOTP = async (email: string, token: string, type: "email" | "r
   const { data, error } = await supabase.auth.verifyOtp({ email: email, token: token, type: type });
   if (error) return { user: null, error };
 
-  
   // add user to user table
   if (data.user) {
     console.log(data.user);
@@ -70,7 +69,7 @@ export const verifyOTP = async (email: string, token: string, type: "email" | "r
     const user = {
       id: data.user.id,
       email: data.user.email,
-      role: role ? role[0].role : "unknown"
+      role: role ? role[0].role : "unknown",
     };
     return { user, error };
   } else return { user: null, error };
@@ -81,7 +80,7 @@ export const fetchRSVPAndAttended = async (email: string) => {
     return {
       rsvp: data[0].rsvp ? data[0].rsvp : [],
       attended: data[0].attended ? data[0].attended : [],
-      error: null
+      error: null,
     };
   } else return { rsvp: null, attended: null, error };
 };
@@ -115,7 +114,18 @@ export const logAttendance = async (selection: number, id: string, userInput: st
   const { error } = await supabase.rpc("validate_attendance", {
     event_id: selection,
     input: userInput,
-    user_id: id
+    user_id: id,
   });
+  if (!error) {
+    const { data, error } = await supabase.from("Users").select("attended").eq("uuid", id);
+    if (!error) {
+      const currAttended = data[0].attended;
+      const { error } = await supabase
+        .from("Users")
+        .update({ attended: [...currAttended, selection] })
+        .eq("uuid", id);
+      if (error) return error;
+    }
+  }
   return error;
 };
